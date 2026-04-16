3d3d61269493e5c9430e5a19d32877a225148042
pyj-0313 <pyjun0313@gmail.com>
2026-04-16 16:37:11 +0900

    01 DAY - JAVA01(진수)

--- JAVA01/src/Ch00/Helloworld.java
+++ JAVA01/src/Ch00/Helloworld.java
@@ -1,11 +1,23 @@
-package Ch00;
+package Ch00;	// 패키지명
 
-public class Helloworld {
+public class Helloworld //클래스 선언부 - 객체지향문법 적용영역
+{
 
-	public static void main(String[] args) {
-		System.out.println("Hello world");
+	// 메서드종류(함수)종류
+	// 라이브러리 메서드		: 미리 만들어져 제공되는 메서드
+	// 사용자정의 메서드		: 개발자에 의해 만들어지는 메서드
+	// main 메서드		: 최초 실행되는 메서드
+	
+	public static void main(String[] args) //main 메서드(함수) 선언부 - 절차지향문법 적용영역
+	{
+		System.out.println("Hello world"); //라이브러리 메서드
 		
-
+		
 	}
+	
+	//글자크기조정 : ctrl + '+' or '-'
+	//주석처리 : ctrl + '/'
+	//한줄복사 : ctrl + alt + '↓'
+	//한줄삭제 : ctrl + d
 
 }
