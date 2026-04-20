f7b6ed91deb752ea98d0032249e943ec1863c519
pyj-0313 <pyjun0313@gmail.com>
2026-04-20 15:17:28 +0900

    03 DAY - JAVA01(TypeChange/정수 실수연산식 자동 형변환 문제-해설)

--- JAVA01/src/Ch02/C04TypeChange.java
+++ JAVA01/src/Ch02/C04TypeChange.java
@@ -15,7 +15,19 @@ public class C04TypeChange {
 		byte var1 = 10;
 		int var2 = 100;
 		long var3 = 1000L;
-		int result = (int)(var1 + var2 + var3); // 왜? var3는 int값 보다 큰 값으로 long으로 자동형변환 되기 때문에 결과값을 int로 강제형변환 시켜야 한다.
+		int result = (int)(var1 + var2 + var3); // 왜? var3는 int값 보다 큰 값으로 long으로 자동형변환 되기 때문에 결과값을 int로 강제형변환 시켜야 한다
+
+		// 실수 연산식
+		// 큰 타입으로 자동 형 변환
+		//byte > short,char > int > long > float > double
+		int intvar = 10;
+		float flvar = 3.3F;
+		double dbvar = 5.5;
+		int result3 = (int)(intvar + flvar + dbvar);	//intvar , flvar 가 각각 double 형으로 형변환됨
+		double result4 = intvar + flvar + dbvar;
+		System.out.println(result3); // result3 값을 int형으로 강제 형변환하여 정수로만 표현하기 때문에 10 + 3 + 5 = 18
+		
+
 	}
 
 }
