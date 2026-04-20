304652c53609ce68735e2e69663ab95c85203186
pyj-0313 <pyjun0313@gmail.com>
2026-04-20 15:46:41 +0900

    03 DAY - JAVA01(TypeChange/String-문자열->숫자형변환(정수))

--- JAVA01/src/Ch02/C07TypeChange_String.java
+++ JAVA01/src/Ch02/C07TypeChange_String.java
@@ -6,33 +6,34 @@ public class C07TypeChange_String {
 		//-------------------------
 		// 문자열 + 문자열
 		//-------------------------
-		System.out.println("문자열1" + "문자열2");			// 문자열 + 문자열
-		System.out.println("문자열1" + ',' + "문자열2");	// 문자열 + 문자 + 문자열
-		System.out.println("문자열1" + 2);				// 문자열 + 숫자 ->숫자를 문자열로 해석! 단순연결
-		System.out.println( "문자열"+','+'!');
-		System.out.println(','+'!'+"문자열");				// (int)',' = 44 / (int)'!' = 33
-		System.out.println("문자열1" + "문자열2");
+//		System.out.println("문자열1" + "문자열2");			// 문자열 + 문자열
+//		System.out.println("문자열1" + ',' + "문자열2");	// 문자열 + 문자 + 문자열
+//		System.out.println("문자열1" + 2);				// 문자열 + 숫자 ->숫자를 문자열로 해석! 단순연결
+//		System.out.println( "문자열"+','+'!');
+//		System.out.println(','+'!'+"문자열");				// (int)',' = 44 / (int)'!' = 33
+//		System.out.println("문자열1" + "문자열2");
 //		//------------------------
 //		// 문자열 -> 숫자형 변환
 //		//------------------------
 		
-		// 문자열 -> 숫자형으로 변환(정수)
-//		int n1 = "10";
-		
-		System.out.println("10"+"20");
-		int n1 = Integer.parseInt("10");
-		int n2 = Integer.parseInt("20");
-		System.out.println(n1+n2);
-		
-		// 문자열 -> 숫자형으로 변환(실수)
-		double n3 = Double.parseDouble("10.5");
-		double n4 = Double.parseDouble("20.4");
-		System.out.println(n3+n4);
-		
-		short n5 = Short.parseShort("5");
-		short n6 = Short.parseShort("6");
-		
-		System.out.println(n5+n6);
+		// 문자열 -> 숫자형으로 변환(정수) - wrapper class
+//		int n1 = (int)"10"; 					//우리가 알던 형변환 사용x
+//		int n1 = new Integer("10") + 30;		//wrapper class 형으로 변환 					
+//		
+//		System.out.println("10"+"20"); 			//1020 문자열 연결 
+//		int n1 = Integer.parseInt("10");		//웹개발코드시 폼으로부터 전달받은 값은 다 문자열임
+//		int n2 = Integer.parseInt("20");
+//		System.out.println(n1+n2); 				//30
+//		
+//		// 문자열 -> 숫자형으로 변환(실수)
+//		double n3 = Double.parseDouble("10.5");
+//		double n4 = Double.parseDouble("20.4");
+//		System.out.println(n3+n4); 				//30.9
+//		
+//		short n5 = Short.parseShort("5");
+//		short n6 = Short.parseShort("6");
+//		
+//		System.out.println(n5+n6); 				//11
 	}
 
 }
