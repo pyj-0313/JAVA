b0353a001373f6a9bf84d54eb6ec2786dc574e0f
pyj-0313 <pyjun0313@gmail.com>
2026-04-20 17:23:17 +0900

    03 DAY - JAVA01(Scanner_주의사항)

--- JAVA01/src/Ch03/C02Scanner.java
+++ JAVA01/src/Ch03/C02Scanner.java
@@ -16,9 +16,14 @@ public class C02Scanner {
 		double num2 = sc.nextDouble();
 		System.out.println("입력된 실수 값 : "+num2);
 		
-		sc.close();
+		System.out.print("문자열 입력 :");
+//		String str1 = sc.next();		//문자열 입력 받기 함수, 띄어쓰기는 포함X
+//		System.out.println("입력된 문자열 : "+str1);
+		sc.nextLine();					// \n이 잔류하고 있기 때문에 찌꺼기를 먹어치워줘야한다
+		String str1 = sc.nextLine();		//문자열 입력 받기 함수, 띄어쓰기는 포함o , 첫번째로 쓸때는 찌꺼기가 남지 않는다.
+		System.out.println("입력된 문자열 : "+str1);
 		
-
+		sc.close();
 	}
 
 }
