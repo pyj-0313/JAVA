26670b820feaf7a574fd5d63b30113b2ce6e4ec2
pyj-0313 <pyjun0313@gmail.com>
2026-04-20 15:33:23 +0900

    03 DAY - JAVA01(TypeChange/String)

---
+++ JAVA01/src/Ch02/C07TypeChange_String.java
@@ -0,0 +1,18 @@
+package Ch02;
+
+public class C07TypeChange_String {
+
+	public static void main(String[] args) {
+		//-------------------------
+		// 문자열 + 문자열
+		//-------------------------
+		System.out.println("문자열1" + "문자열2");			// 문자열 + 문자열
+		System.out.println("문자열1" + ',' + "문자열2");	// 문자열 + 문자 + 문자열
+		System.out.println("문자열1" + 2);				// 문자열 + 숫자 ->숫자를 문자열로 해석! 단순연결
+		System.out.println( "문자열"+','+'!');
+		System.out.println(','+'!'+"문자열");
+		System.out.println("문자열1" + "문자열2");
+
+	}
+
+}
