91767b1ad7a6f017caef1842542739ecd2e38e09
pyj-0313 <pyjun0313@gmail.com>
2026-04-20 17:08:20 +0900

    03 DAY - JAVA01(Scanner_주의사항)

--- JAVA01/src/Ch03/C02Scanner.java
+++ JAVA01/src/Ch03/C02Scanner.java
@@ -1,9 +1,23 @@
 package Ch03;
 
+import java.util.Scanner;
+
 public class C02Scanner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("정수 입력 :");
+		int num1 = sc.nextInt();
+		System.out.println("입력된 정수 값 : "+num1);
+		
+		System.out.print("실수 입력 :");
+		double num2 = sc.nextDouble();
+		System.out.println("입력된 실수 값 : "+num2);
+		
+		sc.close();
+		
 
 	}
 
