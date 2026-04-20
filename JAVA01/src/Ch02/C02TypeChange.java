ee615c81a3712d8cacbdde5eb03d21d25fe8fd73
pyj-0313 <pyjun0313@gmail.com>
2026-04-20 14:46:20 +0900

    03 DAY - JAVA01(TypeChange/강제형변환 개념문제학습)

--- JAVA01/src/Ch02/C02TypeChange.java
+++ JAVA01/src/Ch02/C02TypeChange.java
@@ -8,17 +8,17 @@ public class C02TypeChange {
 		//기본적으로 불가능하기 때문에 강제로 자료형을 바꾸어 전달한다.
 		//데이터 손실의 염려가 있다.
 		int intValue = 20000;
-		char charValue = intValue;
-		System.out.println(charValue);
+		char charValue = (char)intValue;
+		System.out.println(charValue); // int는 4byte이고 char는 2byte이므로 큰 값을 작은 값에 넣을 수 없어 오류가 발생 / char로 강제 형변환
 		
 		long longValue = 500;
-		intValue = longValue;
-		System.out.println(intValue);
+		intValue = (int)longValue;
+		System.out.println(intValue); // long은 8byte이고 int는 4byte이므로 큰 값을 작은 값에 넣을 수 없어 오류가 발생 / int를 사용해서 강제 형변환
 		
 		//데이터 손실!
 		double doubleValue = 3.14;
 		intValue = (int)doubleValue;
-		System.out.println(intValue);
+		System.out.println(intValue); // double을 int로 강제 형변환해서 해결 
 
 	}
 
