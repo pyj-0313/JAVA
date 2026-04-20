c73472a2d5f3419ccad925554d685964397bf0a6
pyj-0313 <pyjun0313@gmail.com>
2026-04-20 15:37:36 +0900

    03 DAY - JAVA01(TypeChange/String-개념잡기)

--- JAVA01/src/Ch02/C07TypeChange_String.java
+++ JAVA01/src/Ch02/C07TypeChange_String.java
@@ -10,7 +10,7 @@ public class C07TypeChange_String {
 		System.out.println("문자열1" + ',' + "문자열2");	// 문자열 + 문자 + 문자열
 		System.out.println("문자열1" + 2);				// 문자열 + 숫자 ->숫자를 문자열로 해석! 단순연결
 		System.out.println( "문자열"+','+'!');
-		System.out.println(','+'!'+"문자열");
+		System.out.println(','+'!'+"문자열");				// (int)',' = 44 / (int)'!' = 33
 		System.out.println("문자열1" + "문자열2");
 
 	}
