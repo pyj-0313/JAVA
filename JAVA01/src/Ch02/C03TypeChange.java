10fd28b4f4e752f85db0eeecfc2b9f7b841dd6c3
pyj-0313 <pyjun0313@gmail.com>
2026-04-20 15:02:36 +0900

    03 DAY - JAVA01(TypeChange/강제형변환 문제해설)

--- JAVA01/src/Ch02/C03TypeChange.java
+++ JAVA01/src/Ch02/C03TypeChange.java
@@ -3,15 +3,14 @@ package Ch02;
 public class C03TypeChange {
 
 	public static void main(String[] args) {
-		int num1 = 129;
-		int num2 = 130;
+		int num1 = 129;			//00000000 00000000 00000000 10000001
+		int num2 = 130;			//00000000 00000000 00000000 10000010
 		
-		byte ch1 = (byte)num1;
-		byte ch2 = (byte)num2;
+		byte ch1 = (byte)num1;  //10000001 -128 + 1 = -127
+		byte ch2 = (byte)num2;  //10000010 -128 + 2 = -126
 		
 		System.out.printf("%d\n", ch1);
 		System.out.printf("%d\n", ch2);
-
 	}
 
 }
