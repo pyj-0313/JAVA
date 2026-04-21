a6462dfa382eed124f74ac8b16d15fcdc7228d36
pyj-0313 <pyjun0313@gmail.com>
2026-04-21 09:21:11 +0900

    03 DAY - JAVA01(Scanner_연습문제-해설)

--- JAVA01/src/Ch03/C03Scanner.java
+++ JAVA01/src/Ch03/C03Scanner.java
@@ -17,15 +17,16 @@ public class C03Scanner {
 		System.out.printf("%s 님의 나이를 입력하세요 : ",name);
 		int age = sc.nextInt();
 		
-		sc.nextLine();
+		
 		// 홍길동 님의 주소를 입력하세요? 대구광역시 달서구 000
 		System.out.printf("%s 님의 주소를 입력하세요 : ",name);
+		sc.nextLine();
 		String addr = sc.nextLine();
 		
 		// 홍길동 님의 나이는 35세 주소는 대구광역시 ~~ 입니다
 		System.out.printf("%s 님의 나이는 %d세 주소는 %s 입니다",name,age,addr);
 		
-		
+		sc.close();
 		
 
 	}
