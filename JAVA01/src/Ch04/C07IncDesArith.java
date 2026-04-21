0eb982af6597d3eb20684ca6034a85a6f1a7df1f
pyj-0313 <pyjun0313@gmail.com>
2026-04-21 11:26:01 +0900

    04 DAY - JAVA01(DesArith)

--- JAVA01/src/Ch04/C07IncDesArith.java
+++ JAVA01/src/Ch04/C07IncDesArith.java
@@ -5,12 +5,12 @@ public class C07IncDesArith {
 		int a=5, b=6, c=10, d;
 		boolean e;
 		d=++a*b--;
-		 System.out.printf("a = %d, b = %d, d = %d\n", a, b, d);
+		 System.out.printf("a = %d, b = %d, d = %d\n", a, b, d); // a=6 b=5 d=36
 		d=a++ + ++c - b--;
-		System.out.printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+		System.out.printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d); // a=7 b=4 c=11 d=12
 		a=1;
 		b=0;
 		e=(a++>0)||((b*d/c)>0);
-		System.out.printf("a = %d, b = %d, c = %d, d = %d ,e = %b\n", a, b, c, d , e);
+		System.out.printf("a = %d, b = %d, c = %d, d = %d ,e = %b\n", a, b, c, d , e); // a=2 b=0 c=11 d=12 e=true 
 	}
 }	
