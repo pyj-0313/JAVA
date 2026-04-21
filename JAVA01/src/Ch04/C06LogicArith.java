349eb5029a6f4a2f44c1c5d3da4f63548331db0b
pyj-0313 <pyjun0313@gmail.com>
2026-04-21 11:25:49 +0900

    04 DAY - JAVA01(LogicArith)

--- JAVA01/src/Ch04/C06LogicArith.java
+++ JAVA01/src/Ch04/C06LogicArith.java
@@ -4,24 +4,24 @@ public class C06LogicArith {
 	public static void main(String[] args) {
 		int a=0,b=0;
 		boolean c;
-		a = 1;
-		b = 1;
-		c =(++a>0)||(++b>0) ;
-		System.out.printf("a = %d , b = %d c = %s\n", a, b, c);
+//		a = 1;
+//		b = 1;
+//		c =(++a>0)||(++b>0) ; // 왼쪽이 true면 오른쪽은 진행x
+//		System.out.printf("a = %d , b = %d c = %s\n", a, b, c);
 
-		a = 1;
-		b = 1;
-		c = (++a>0)||(--b>0) ;
-		System.out.printf("a = %d , b = %d c = %s\n", a, b, c);
-
-		a = 1;
-		b = 1;
-		c = (--a>0)||(++b>0) ;
-		System.out.printf("a = %d , b = %d c = %s\n", a, b, c);
-
-		a = 1;
-		b = 1;
-		c = (--a>0)||(b-->0) ;
-		System.out.printf("a = %d , b = %d c = %s\n", a, b, c);
+//		a = 1;
+//		b = 1;
+//		c = (++a>0)||(--b>0) ;
+//		System.out.printf("a = %d , b = %d c = %s\n", a, b, c); // a=2 b=1 c=true
+//
+//		a = 1;
+//		b = 1;
+//		c = (--a>0)||(++b>0) ;
+//		System.out.printf("a = %d , b = %d c = %s\n", a, b, c); // a=0 b=2 c=true
+//
+//		a = 1;
+//		b = 1;
+//		c = (--a>0)||(b-->0) ;
+//		System.out.printf("a = %d , b = %d c = %s\n", a, b, c); // a=0 b=0 c=true
 	}
 }	
