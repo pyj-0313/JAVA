424b9da38401afd94b43c35195b51f6be30fc73b
pyj-0313 <pyjun0313@gmail.com>
2026-04-23 12:40:10 +0900

    06 DAY - JAVA01(CONTINUE_BREAK_flag탈출)

---
+++ JAVA01/src/Ch06/C03CONTINUE_BREAK.java
@@ -0,0 +1,69 @@
+package Ch06;
+
+import java.util.Scanner;
+
+public class C03CONTINUE_BREAK {
+
+	public static void main(String[] args) {
+		//---------------------------
+		// 1 break
+		//---------------------------
+		// -1을 입력하기 전까지 정수값을 누적하여 합을 구하고 출력
+//		Scanner sc = new Scanner(System.in);
+//		int n;
+//		int sum = 0;
+//		while(true) {
+//			
+//			n=sc.nextInt();
+//			if(n==-1)
+//				break;
+//			
+//			sum+=n;
+//		}
+//		System.out.println("SUM : " + sum);
+//		
+//		sc.close();
+		
+		//-----------------------------------
+		// while(true) x
+		//-----------------------------------
+//		Scanner sc = new Scanner(System.in);
+//		int n;
+//		int sum = 0;
+//		while((n=sc.nextInt())!=-1)
+//			sum+=n;
+//		System.out.println("SUM : " + sum);
+//		
+//		sc.close();
+
+		//---------------------------------
+		// 모든 반복문 탈출 : flag = 7 x 7 까지만(dan==7 && i==7)
+		//---------------------------------
+//		int dan = 2;
+//		boolean isExit = false; //나갈꺼다
+//		while(dan<10) {
+//			
+//			int i=1;
+//			while(i<10) {
+//				System.out.printf("%d x %d = %d\n", dan,i,dan*i);
+//				
+//				if(dan==7 && i==7) {
+//					isExit = true;
+//					break;
+//				}
+//				i++;
+//			}
+//			
+//			if(isExit)
+//				break;
+//			System.out.println();
+//			dan++;
+//		}
+		
+
+		
+		
+		
+	}
+
+}
