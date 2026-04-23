4cf5a4cf7c996226afc81bce3af2e790f2f2bca5
pyj-0313 <pyjun0313@gmail.com>
2026-04-23 17:06:03 +0900

    06 DAY - JAVA02(C01Person)

---
+++ JAVA02/src/Ch01/C01Person.java
@@ -0,0 +1,12 @@
+package Ch01;
+
+public class C01Person {
+	//속성(멤버변수,필드)
+	public String name;		//null(기본값)
+	public int age;			// 0(기본값)
+	public float height;	// 0.0F(기본값) 
+	public double weight;	// 0.0(기본값)
+	
+	//기능(메서드,함수..)
+	
+}
