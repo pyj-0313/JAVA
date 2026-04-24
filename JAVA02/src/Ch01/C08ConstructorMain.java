1110d2860005fff202098ec9c6d192313b80b4b9
pyj-0313 <pyjun0313@gmail.com>
2026-04-24 10:19:08 +0900

    07 DAY - JAVA01(Constructor)

--- JAVA02/src/Ch01/C08ConstructorMain.java
+++ JAVA02/src/Ch01/C08ConstructorMain.java
@@ -1,6 +1,7 @@
 package Ch01;
 
 // 생성자 메서드
+
 // ex) Scanner sc = new Scanner(System.in)
 // 생성자 메서드위치 : Scanner(System.in)
 //
@@ -16,43 +17,62 @@ package Ch01;
 // 클래스 내에 생성자함수를 명시하면(코드삽입o)
 // 디폴트 생성자는 주입되지 않는다
 
-
-class C08Simple
-{
-	//속성
+class C08Simple {
+	// 속성
 	int n1;
 	double n2;
 	boolean n3;
 	String n4;
-	//기능
+	// 기능
 
-	
-	//생성자
-	C08Simple(){
+	// 생성자
+	C08Simple() {
 		System.out.println("C08Simple()");
 		this.n1 = 11;
 		this.n2 = 22.22;
 		this.n3 = true;
 		this.n4 = "HELLO WORLD";
 	}
-	
-	
-	C08Simple(int n1,double n2,boolean n3,String n4){
-		System.out.println("C08Simple(int n1,double n2,boolean n3,String n4)");
+
+//	C08Simple(int n1,double n2,boolean n3,String n4){
+//		System.out.println("C08Simple(int n1,double n2,boolean n3,String n4)");
+//		this.n1 = n1;
+//		this.n2 = n2;
+//		this.n3 = n3;
+//		this.n4 = n4;
+//	}
+
+	public C08Simple(int n1) {
+		super();
+		this.n1 = n1;
+	}
+
+	public C08Simple(int n1, double n2) {
+		super();
+		this.n1 = n1;
+		this.n2 = n2;
+	}
+
+	public C08Simple(int n1, double n2, boolean n3) {
+		super();
+		this.n1 = n1;
+		this.n2 = n2;
+		this.n3 = n3;
+	}
+
+	public C08Simple(int n1, double n2, boolean n3, String n4) {
+		super();
 		this.n1 = n1;
 		this.n2 = n2;
 		this.n3 = n3;
 		this.n4 = n4;
-		
 	}
 	
-	//toString재정의
+	// toString재정의
 	@Override
 	public String toString() {
 		return "C08Simple [n1=" + n1 + ", n2=" + n2 + ", n3=" + n3 + ", n4=" + n4 + "]";
 	}
-	
-
 
 }
 
@@ -63,7 +83,7 @@ public class C08ConstructorMain {
 		System.out.println(ob1);
 //		System.out.println(ob1.toString()); // 이렇게 사용해도 됨
 		System.out.println("--------------------");
-		C08Simple ob2 = new C08Simple(10,20.2,true,"TEST");
+		C08Simple ob2 = new C08Simple(10, 20.2, true, "TEST");
 		System.out.println(ob2);
 	}
 
