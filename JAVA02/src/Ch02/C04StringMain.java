cdb30cc6462cf6603679a1a8a7d3085df0f206f8
pyj-0313 <pyjun0313@gmail.com>
2026-04-24 17:25:32 +0900

    07 DAY - JAVA02(Sring)

---
+++ JAVA02/src/Ch02/C04StringMain.java
@@ -0,0 +1,27 @@
+package Ch02;
+
+public class C04StringMain {
+
+	public static void main(String[] args) {
+		String str1 = "java";
+		String str2 = "java";
+		String str3 = new String("java");
+		String str4 = new String("java");
+		
+		System.out.printf("%X\n",System.identityHashCode(str1));
+		System.out.printf("%X\n",System.identityHashCode(str2));
+		System.out.printf("%X\n",System.identityHashCode(str3));
+		System.out.printf("%X\n",System.identityHashCode(str4));
+		System.out.println("----------------------");
+		System.out.println("str1==str2 ? " + (str1==str2)); //상수 POOL
+		System.out.println("str3==str4 ? " + (str3==str4)); //힙 메모리공간
+		System.out.println("str1==str3 ? " + (str1==str3));
+		System.out.println("str2==str4 ? " + (str2==str4));
+		System.out.println("----------------------");
+		System.out.println("str1==str2 ? " + (str1.equals(str2)));
+		System.out.println("str3==str4 ? " + (str3.equals(str4)));
+		System.out.println("str1==str3 ? " + (str1.equals(str3)));
+		System.out.println("str2==str4 ? " + (str2.equals(str4)));
+	}
+
+}
