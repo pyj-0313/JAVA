078aee43bf0fc98b0a55f38224766c28c8bdf05c
pyj-0313 <pyjun0313@gmail.com>
2026-04-24 16:04:46 +0900

    07 DAY - JAVA01(정보은닉)

---
+++ JAVA02/src/Ch02/C01정보은닉.java
@@ -0,0 +1,17 @@
+package Ch02;
+
+//접근 한정자
+//멤버/클래스의 접근범위를 제한을 위한 예약어
+//public		: 모든 클래스에서 접근가능
+//private		: 해당 클래스에서만 접근가능
+//protected		: 상속 관계를 가지는 경우에 접근가능
+//default(기본)	: 동일 패키지에 속한 클래스에서 접근가능
+
+public class C01정보은닉 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
