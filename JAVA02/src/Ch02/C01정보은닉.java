008320c1cd20e258229aa56bfbdf6e819a5abcc6
pyj-0313 <pyjun0313@gmail.com>
2026-04-24 16:23:06 +0900

    07 DAY - JAVA01(정보은닉_getter and setter)

--- JAVA02/src/Ch02/C01정보은닉.java
+++ JAVA02/src/Ch02/C01정보은닉.java
@@ -7,10 +7,55 @@ package Ch02;
 //protected		: 상속 관계를 가지는 경우에 접근가능
 //default(기본)	: 동일 패키지에 속한 클래스에서 접근가능
 
+
+class C01Person{
+	//속성
+	private String name;
+	private int age;
+	private String addr;
+	
+	//생성자(모든인자 받는)
+	public C01Person(String name, int age, String addr) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.addr = addr;
+	}
+	//기능
+
+	//getter and setter 함수 (private 설정한 클래스 값을 변경가능, 카멜표기법)
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	//toString() 재정의
+	@Override
+	public String toString() {
+		return "C01Person [name=" + name + ", age=" + age + ", addr=" + addr + "]";
+	}
+	
+}
+
+
 public class C01정보은닉 {
 
 	public static void main(String[] args) {
+		C01Person ob = new C01Person("홍길동",55,"대구");
 		
+//		ob.name="남길동"; //private 설정으로 인한 class 접근 불가능
+		ob.setName("남길동"); //홍길동 -> 남길동으로 수정
+		
+		
+		System.out.println(ob);
 
 	}
 
