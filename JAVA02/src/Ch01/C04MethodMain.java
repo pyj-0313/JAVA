0906169fa28be1b34ac051f1335432bd07280fb7
pyj-0313 <pyjun0313@gmail.com>
2026-04-27 14:26:55 +0900

    오류 해결

--- JAVA02/src/Ch01/C04MethodMain.java
+++ JAVA02/src/Ch01/C04MethodMain.java
@@ -1,35 +1,35 @@
-package Ch01;
-
-import java.util.Scanner;
-
-class Cal{
-	//속성
-	Scanner sc = new Scanner(System.in);
-	
-	//기능
-	public int sum1(int n1, int n2) {
-		return n1 + n2;
-	}
-	public int sum2() {
-		return sc.nextInt() + sc.nextInt();
-	}
-	public void sum3(int n1, int n2) {
-		System.out.print(n1 + n2);
-	}
-	public void sum4() {
-		System.out.println(sc.nextInt() + sc.nextInt());
-	}
-}
-
-public class C04MethodMain {
-
-	public static void main(String[] args) {
-		Cal cal = new Cal();
-		System.out.println("SUM1 : " + cal.sum1(10,20));
-		System.out.println("SUM2 : "); System.out.println(cal.sum2()); 
-		System.out.print("SUM3 : "); cal.sum3(100, 200);
-		System.out.print("SUM4 : "); cal.sum4();
-
-	}
-
-}
+//package Ch01;
+//
+//import java.util.Scanner;
+//
+//class Cal{
+//	//속성
+//	Scanner sc = new Scanner(System.in);
+//	
+//	//기능
+//	public int sum1(int n1, int n2) {
+//		return n1 + n2;
+//	}
+//	public int sum2() {
+//		return sc.nextInt() + sc.nextInt();
+//	}
+//	public void sum3(int n1, int n2) {
+//		System.out.print(n1 + n2);
+//	}
+//	public void sum4() {
+//		System.out.println(sc.nextInt() + sc.nextInt());
+//	}
+//}
+//
+//public class C04MethodMain {
+//
+//	public static void main(String[] args) {
+//		Cal cal = new Cal();
+//		System.out.println("SUM1 : " + cal.sum1(10,20));
+//		System.out.println("SUM2 : "); System.out.println(cal.sum2()); 
+//		System.out.print("SUM3 : "); cal.sum3(100, 200);
+//		System.out.print("SUM4 : "); cal.sum4();
+//
+//	}
+//
+//}
