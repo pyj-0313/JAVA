ffc1010b9eba2830bbbdaf86b1b50880c2fac51a
pyj-0313 <pyjun0313@gmail.com>
2026-04-28 12:43:46 +0900

    09 DAY - JAVA02(C01InheritanceMain_상속)

---
+++ JAVA02/src/Ch05/C01InheritanceMain.java
@@ -0,0 +1,44 @@
+package Ch05;
+
+
+class Point2D{
+	int x;
+	int y;
+	
+	//디폴트생성자
+	Point2D(){
+		System.out.println("Point2D 디폴트생성자 호출!");
+	}
+	//toString() 재정의
+	@Override
+	public String toString() {
+		return "Point2D [x=" + x + ", y=" + y + "]";
+	}
+}
+class Point3D extends Point2D{
+	
+	int z;
+	
+	Point3D(){
+		System.out.println("Point3D 디폴트생성자 호출!");
+	}
+	//toString() 재정의 //상속관계가 되어 있으면 필드값을 가져올 수 있다
+	@Override
+	public String toString() {
+		return "Point3D [z=" + z + ", x=" + x + ", y=" + y + "]";
+	}
+	
+	
+}
+public class C01InheritanceMain {
+
+	public static void main(String[] args) {
+		
+		Point3D ob = new Point3D();
+		ob.x=10;
+		ob.y=20;
+		ob.z=30;
+		System.out.println(ob);
+	}
+
+}
