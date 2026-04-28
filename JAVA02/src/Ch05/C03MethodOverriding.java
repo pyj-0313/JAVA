a3dc34d2014e299d499ed8374fce71fa093e550e
pyj-0313 <pyjun0313@gmail.com>
2026-04-28 16:32:26 +0900

    09 DAY - JAVA02(C03,C04_오버로딩,라이딩)

---
+++ JAVA02/src/Ch05/C03MethodOverriding.java
@@ -0,0 +1,52 @@
+package Ch05;
+
+//함수재정의(Method Overriding)
+// 상속관계를 전제로 상위클래스의 메서드를 하위클래스가 재정의(함수의Body를 고쳐사용)하는 것을 허용한 문법
+// 메서드의 헤더부분은 동일하고 본문({}) 의 로직을 수정할 수 있게 허용함으로서 다양한 형태의 객체가 형성 될 수 있도록 유도(다형성의 근거가 될 수 있는 문법 중 하나)
+// 상위 클래스로 부터 동일한 메서드를 물려받더라도 각 하위 클래스마다 다른기능구현의 결과물을 만들어낼 수 있다.
+
+// 오버로딩 vs 오버라이딩
+
+// 오버 로딩
+// 1) 상속 전제? x
+// 2) 함수 헤더 변경 ? o 반환자료형 고려사항아님, 함수명 고정, 파라미터 변동
+// 3) 함수 본문 변경 ? x 
+// 4) 목적? 개발편의성에 중점(함수의 이름은 고정)
+
+// 오버라이딩
+// 1) 상속 전제 ? o 상속관계가 있어야 사용가능하다는 의미
+// 2) 함수 헤더 변경 ? x 함수헤더는 건들지 않는다 본문만 건듬
+// 3) 함수 본문 변경 ? o
+// 4) 목적 ? 다형성
+
+class Animal{
+	void sound() {System.out.println("소리낸다!");} //기능
+}
+
+class Dog extends Animal{
+	void sound() {System.out.println("멍멍 소리낸다!");};
+}
+
+class Cat extends Animal{
+
+	@Override
+	void sound() {
+		System.out.println("냐옹 소리낸다!!");
+	}
+	
+}
+
+
+public class C03MethodOverriding {
+
+	public static void main(String[] args) {
+		Dog tori = new Dog();
+		tori.sound();
+		System.out.println("---------");
+		Cat navi = new Cat();
+		navi.sound();
+		
+
+	}
+
+}
---
+++ JAVA02/src/Ch05/C04OverLoadRiddingMain.java
@@ -0,0 +1,46 @@
+package Ch05;
+
+
+class Super{
+	
+	int sum(int x, int y) {
+		return x + y;
+	}
+	
+	//함수 오버로딩
+	int sum(int x, int y, int z) {
+		return x + y + z;
+	}
+}
+
+class Sub extends Super{
+	//오버로딩인지 아닌지 o,x 표시
+	//오버라이딩
+	int sum(int x, int y) {
+		return x + y + 1;
+	}
+	//오버라이딩
+	int sum(int x, int y, int z) {
+		return x+x+y+y+z+z;
+	}
+	//오버로딩
+	int sum(int x, int y, int z, int h) {
+		return x + y + z + h;
+	}오버로딩
+	int sum(int x, double y) {
+		return (int)
+	}
+	//오버라이딩
+	double sum(int x, double y, double z) {
+		return (int)(x + y + z);
+	}
+}
+
+public class C04OverLoadRiddingMain {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
