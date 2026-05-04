58a9270b9dd0e4ba9d4563fdf921a46880a57863
pyj-0313 <pyjun0313@gmail.com>
2026-05-04 11:33:34 +0900

    11 DAY - JAVA03(C02ArrayIndexBoundExceptionMain(예외처리))

---
+++ JAVA03/src/Ch02/C02ArrayIndexBoundExceptionMain.java
@@ -0,0 +1,24 @@
+package Ch02;
+
+public class C02ArrayIndexBoundExceptionMain {
+
+	public static void main(String[] args) {
+		
+		try {
+			int[] arr = { 10, 20, 30 };
+			System.out.println(arr[0]);
+			System.out.println(arr[1]);
+			System.out.println(arr[2]);
+//			System.out.println(arr[3]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("ArrayIndexOutOfBoundsException 처리!");
+		} finally {
+			System.out.println("예외 발생과는 무관하게 실행되는 코드!");
+		}
+
+		System.out.println("반드시 실행되어야 할 코드1");
+		System.out.println("반드시 실행되어야 할 코드2");
+
+	}
+
+}
