d1ede47d4bb5c30603132a816c9a0cd9927812a4
pyj-0313 <pyjun0313@gmail.com>
2026-05-04 10:47:13 +0900

    11 DAY - JAVA03(C07LocalDateTimeFormatMain(포매터 최근 사용 방식))

--- JAVA03/src/Ch01/C07LocalDateTimeFormatMain.java
+++ JAVA03/src/Ch01/C07LocalDateTimeFormatMain.java
@@ -1,28 +1,37 @@
 package Ch01;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class C07LocalDateTimeFormatMain {
 
 	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("YYYY/MM/DD 입력 : ");
+//		String ymd = sc.next();
+//		//포매터 객체 생성(입력용)
+//		DateTimeFormatter fmtin = DateTimeFormatter.ofPattern("yyy/MM/dd");
+//		LocalDate date = LocalDate.parse(ymd,fmtin);
+//		System.out.println(date);
+//		
+//		//포매터 객체 생성(출력용)
+//		DateTimeFormatter fmtout = DateTimeFormatter.ofPattern("yyyy★MM★dd");
+//		System.out.println("출력포맷 : " + fmtout.format(date));
+		
+//		--------------------------------------------------
 		Scanner sc = new Scanner(System.in);
-		System.out.print("YYYY/MM/DD 입력 : ");
-		String ymd = sc.next();
+		System.out.print("YYYY/MM/DD HH:mm:ss 입력 : ");
+		String ymd = sc.nextLine();
+		
 		//포매터 객체 생성(입력용)
-		DateTimeFormatter fmtin = DateTimeFormatter.ofPattern("yyy/MM/dd");
-		LocalDate date = LocalDate.parse(ymd,fmtin);
+		DateTimeFormatter fmtin = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		LocalDateTime date = LocalDateTime.parse(ymd,fmtin);
 		System.out.println(date);
 		
 		//포매터 객체 생성(출력용)
-		DateTimeFormatter fmtout = DateTimeFormatter.ofPattern("yyyy★MM★dd");
+		DateTimeFormatter fmtout = DateTimeFormatter.ofPattern("yyyy-MM-dd HH-mm-ss");
 		System.out.println("출력포맷 : " + fmtout.format(date));
-		
-		
-		
-		
 		
 
 	}
