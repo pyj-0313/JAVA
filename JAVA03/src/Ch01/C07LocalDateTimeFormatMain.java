918e8f283660b239e8c4247927a4da81328a205a
pyj-0313 <pyjun0313@gmail.com>
2026-05-04 10:36:03 +0900

    11 DAY - JAVA03(C07LocalDateTimeFormatMain(포매터 최근 사용 방식))

---
+++ JAVA03/src/Ch01/C07LocalDateTimeFormatMain.java
@@ -0,0 +1,30 @@
+package Ch01;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class C07LocalDateTimeFormatMain {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("YYYY/MM/DD 입력 : ");
+		String ymd = sc.next();
+		//포매터 객체 생성(입력용)
+		DateTimeFormatter fmtin = DateTimeFormatter.ofPattern("yyy/MM/dd");
+		LocalDate date = LocalDate.parse(ymd,fmtin);
+		System.out.println(date);
+		
+		//포매터 객체 생성(출력용)
+		DateTimeFormatter fmtout = DateTimeFormatter.ofPattern("yyyy★MM★dd");
+		System.out.println("출력포맷 : " + fmtout.format(date));
+		
+		
+		
+		
+		
+
+	}
+
+}
