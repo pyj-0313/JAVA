c29cae8e28fbc90f5bbd5c86879994ee82182684
pyj-0313 <pyjun0313@gmail.com>
2026-05-04 10:26:30 +0900

    11 DAY - JAVA03(C05DateCalendarMain_포매터 객체 생성(입력용))

---
+++ JAVA03/src/Ch01/C06SimpleDateFormatMain.java
@@ -0,0 +1,23 @@
+package Ch01;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class C06SimpleDateFormatMain {
+
+	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("YYYY/MM/DD 입력 : ");
+		String ymd = sc.next();
+		
+		System.out.println("문자열 : " + ymd);
+		
+		//포매터 객체 생성(입력용)
+		SimpleDateFormat fmtin = new SimpleDateFormat("yyy/MM/dd");
+		Date date = fmtin.parse(ymd);
+		System.out.println("fmtin : " + date);
+	}
+
+}
