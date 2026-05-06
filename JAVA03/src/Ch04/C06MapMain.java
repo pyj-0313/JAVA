fdb81e201e5209d3e055ea4254fd74b676733909
pyj-0313 <pyjun0313@gmail.com>
2026-05-06 11:33:15 +0900

    12 DAY - JAVA03(C06MapMain_삭제)

--- JAVA03/src/Ch04/C06MapMain.java
+++ JAVA03/src/Ch04/C06MapMain.java
@@ -13,11 +13,16 @@ public class C06MapMain {
 		map.put("aaa",1111);
 		map.put("bbb",2222);
 		map.put("ccc",3333);
-		map.put("ddd",4444);		//
+		map.put("ddd",4444);		// older
+		map.put("ddd",5555);		// newer
 		
 		//확인
 		for(String key : map.keySet()) //["aaa","bbb","ccc","ddd"]
 			System.out.println("KEY : " + key + " VALUE : " + map.get(key));
+
+		//삭제
+		map.remove("aaa");
+		
 
 	}
 
