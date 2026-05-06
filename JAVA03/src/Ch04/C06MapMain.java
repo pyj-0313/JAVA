511f5afea8dda48b7d577908d80c741d3a18609f
pyj-0313 <pyjun0313@gmail.com>
2026-05-06 11:28:29 +0900

    12 DAY - JAVA03(C06MapMain_추가)

---
+++ JAVA03/src/Ch04/C06MapMain.java
@@ -0,0 +1,22 @@
+package Ch04;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class C06MapMain {
+
+	public static void main(String[] args) {
+		
+		Map<String,Integer> map =  new HashMap();
+		
+		//추가
+		map.put("aaa",1111);
+		map.put("aaa",2222);
+		map.put("aaa",3333);
+		map.put("aaa",4444);		//
+		
+
+
+	}
+
+}
