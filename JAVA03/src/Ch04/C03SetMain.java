b2d01e27554165d32b84f6d0444f974f6a2f6d66
pyj-0313 <pyjun0313@gmail.com>
2026-05-06 09:42:57 +0900

    12 DAY - JAVA03(C03SetMain_추가)

---
+++ JAVA03/src/Ch04/C03SetMain.java
@@ -0,0 +1,19 @@
+package Ch04;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class C03SetMain {
+
+	public static void main(String[] args) {
+		Set<String> set = new HashSet();
+		//추가
+		set.add("HTML/CSS/JS");
+		set.add("NODEJS");
+		set.add("SCSS");
+		set.add("REACT");
+		set.add("JAVA");		//older
+
+	}
+
+}
