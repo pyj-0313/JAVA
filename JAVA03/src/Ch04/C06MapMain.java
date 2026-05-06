8cf8c3bb3f2efac687219750275a23932389dcfd
pyj-0313 <pyjun0313@gmail.com>
2026-05-06 12:10:17 +0900

    12 DAY - JAVA03(C06MapMain)

--- JAVA03/src/Ch04/C06MapMain.java
+++ JAVA03/src/Ch04/C06MapMain.java
@@ -14,7 +14,7 @@ public class C06MapMain {
 		map.put("bbb",2222);
 		map.put("ccc",3333);
 		map.put("ddd",4444);		// older
-		map.put("ddd",5555);		// newer
+		map.put("ddd",5555);		// newer(적용)
 		
 		//확인
 		for(String key : map.keySet()) //["aaa","bbb","ccc","ddd"]
