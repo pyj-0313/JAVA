8ca88d1152cd31add01db0c701dc3e161fa9cb81
pyj-0313 <pyjun0313@gmail.com>
2026-05-08 10:55:21 +0900

    13 DAY - JAVA03(C01CharConvertStreamMain_INPUT 보조스트림)

--- JAVA03/src/Ch07/C01CharConvertStreamMain.java
+++ JAVA03/src/Ch07/C01CharConvertStreamMain.java
@@ -1,10 +1,10 @@
 package Ch07;
 
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class C01CharConvertStreamMain {
 
@@ -13,17 +13,31 @@ public class C01CharConvertStreamMain {
 		String str = "문자 변환 스트림테스트!";
 		
 		//OUTPUT 보조스트림
-		OutputStream out = new FileOutputStream("C:\\IOTEST\\test.txt");
-//		out.write(str.getBytes());	//String -> byte[] 변환
-		OutputStreamWriter wout = new OutputStreamWriter(out);
-//		wout.write(str);
-//		wout.flush();
+//		OutputStream out = new FileOutputStream("C:\\IOTEST\\test.txt");
+////		out.write(str.getBytes());	//String -> byte[] 변환
+//		OutputStreamWriter wout = new OutputStreamWriter(out);
+////		wout.write(str);
+////		wout.flush();
+//		
+//		BufferedWriter bout = new BufferedWriter(wout);
+//		bout.write(str);
+//		bout.flush();
+//		
+//		out.close();
 		
-		BufferedWriter bout = new BufferedWriter(wout);
-		bout.write(str);
-		bout.flush();
+	
+		//-----------------
+		// INPUT 보조스트림
+		InputStream in = new FileInputStream("C:\\IOTEST\\test.txt");
+		InputStreamReader rin = new InputStreamReader(in);
+		BufferedReader bin = new BufferedReader(rin);
 		
-		out.close();
+		while(true) {
+			int data = bin.read();
+			if(data==-1)
+				break;
+			System.out.print((char)data);
+		}
 		
 	}
 
