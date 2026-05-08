ed25be306f7538602bc24276d93eee5fde6f09b3
pyj-0313 <pyjun0313@gmail.com>
2026-05-08 11:39:49 +0900

    14 DAY - JAVA03(C04ObjectStreamMain_클래스생성)

--- JAVA03/src/Ch07/C04ObjectStreamMain.java
+++ JAVA03/src/Ch07/C04ObjectStreamMain.java
@@ -1,5 +1,78 @@
 package Ch07;
 
+import java.util.Date;
+
+class Board{
+	private int bno;
+	private String title;
+	private String content;
+	private String writer;
+	private Date date;		//java.util.Date
+	
+	//디폴트생성자
+	Board() {}
+	
+	//모든인자생성자
+	public Board(int bno, String title, String content, String writer, Date date) {
+		super();
+		this.bno = bno;
+		this.title = title;
+		this.content = content;
+		this.writer = writer;
+		this.date = date;
+	}
+	
+	//getter and setter
+	public int getBno() {
+		return bno;
+	}
+
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	//toString  재정의
+	@Override
+	public String toString() {
+		return "Board [bno=" + bno + ", title=" + title + ", content=" + content + ", writer=" + writer + ", date="
+				+ date + "]";
+	}
+	
+}
+
+
 public class C04ObjectStreamMain {
 
 	public static void main(String[] args) {
