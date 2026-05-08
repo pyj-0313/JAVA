7c2fc9e4c8f9037e82745389caaf136e4fc95ec1
pyj-0313 <pyjun0313@gmail.com>
2026-05-08 11:31:41 +0900

    14 DAY - JAVA03(C03DataInOutputStreamMain_데이터저장하기)

--- JAVA03/src/Ch07/C03DataInOutputStreamMain.java
+++ JAVA03/src/Ch07/C03DataInOutputStreamMain.java
@@ -1,9 +1,22 @@
 package Ch07;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class C03DataInOutputStreamMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+//		FileOutputStream out = new FileOutputStream("c:\\IOTEST\\test3.txt");
+//		DataOutputStream dout = new DataOutputStream(out);
+//		dout.writeUTF("홍길동");
+//		dout.writeDouble(95.5);
+//		dout.writeInt(100);
+//		dout.flush();
+//		dout.close();
+		
+
 
 	}
 
