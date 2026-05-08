5cd83c6052ee759057a8fdfdcc309bbc7dc47dd4
pyj-0313 <pyjun0313@gmail.com>
2026-05-08 12:10:48 +0900

    14 DAY - JAVA03(C04ObjectStreamMain)

--- JAVA03/src/Ch07/C04ObjectStreamMain.java
+++ JAVA03/src/Ch07/C04ObjectStreamMain.java
@@ -1,8 +1,16 @@
 package Ch07;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
 import java.util.Date;
 
-class Board{
+class Board implements Serializable		//직렬화 
+{
+	
+	private static final long serialVersionUID = 1L; //(명령어 사용하지 않을 시)클래스가 수정됬을때 문제가 발생할수있다.
+	
 	private int bno;
 	private String title;
 	private String content;
@@ -75,9 +83,42 @@ class Board{
 
 public class C04ObjectStreamMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+//		Board board1 = new Board(1,"제목1","내용1","작성자1",new Date());
+//		Board board2 = new Board(2,"제목2","내용2","작성자2",new Date());
+//		Board board3 = new Board(3,"제목3","내용3","작성자3",new Date());
+//		
+//		FileOutputStream out = new FileOutputStream("C:\\IOTEST\\board.db");
+//		ObjectOutputStream oout = new ObjectOutputStream(out);
+//		oout.writeObject(board1);
+//		oout.writeObject(board2);
+//		oout.writeObject(board3);
+//		oout.flush();
+//		oout.close();
+//		out.close();
 
+		FileInputStream in = new FileInputStream("C:\\IOTEST\\board.db");
+		ObjectInputStream oin = new ObjectInputStream(in);
+		
+		Object obj = oin.readObject();
+		Board down = (Board)obj;
+		System.out.println(down);
+		
+		Object obj2 = oin.readObject();
+		Board down2 = (Board)obj2;
+		System.out.println(down2);
+		
+		Object obj3 = oin.readObject();
+		Board down3 = (Board)obj3;
+		System.out.println(down3);
+		
+		// 더이상 읽어올 OBJECT가 없으면 java.io.EOFException 예외발생(예외처리 후 RuntimeError해결)
+		Object obj4 = oin.readObject();
+		
+		oin.close();
+		in.close();
+		
+		}
 	}
 
 }
