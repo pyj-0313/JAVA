f230082d8bc408a586e4a361b3dd2903b6747165
pyj-0313 <pyjun0313@gmail.com>
2026-05-11 14:14:14 +0900

    15 DAY - JAVA03(C01DBConn_DB접속하기위한 최소단위)

---
+++ JAVA03/src/Ch08/C01DBConn.java
@@ -0,0 +1,23 @@
+package Ch08;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class C01DBConn {
+
+	public static void main(String[] args) {
+		//DB CONN DATA
+		String id = "root";
+		String pw = "1234";
+		String url = "jdbc:mysql://localhost:3306/opendatadb";
+		
+		//JDBC참조변수
+		Connection conn = null;			// DBMS 의 특정 D와 연결되는 객체
+		PreparedStatement pstmt = null;	// SQL Query 전송용 객체
+		ResultSet rs = null;			// Select 결과물 담을 객체
+		
+
+	}
+
+}
