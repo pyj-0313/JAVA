27f9a2310d6207b709d3d37f64364d3407b54f1a
pyj-0313 <pyjun0313@gmail.com>
2026-05-12 09:19:48 +0900

    16 DAY - JAVA03(C01Server_루프접속)

--- JAVA03/src/Ch09/C01Server.java
+++ JAVA03/src/Ch09/C01Server.java
@@ -13,17 +13,31 @@ public class C01Server {
 		//서버 소켓 생성
 		ServerSocket server = new ServerSocket(7000);// IP: 서버IP(현재컴퓨터) PORT : 7000
 		System.out.println("[INFO] SERVER SOCKET LISIENING");
+		
 		//1회접속
+//		Socket client = server.accept();	// 1회
+//		OutputStream out = client.getOutputStream();
+//		DataOutputStream dout = new DataOutputStream(out);
+//		System.out.println("접속자 IP : " + client.getInetAddress());
+//		dout.writeUTF("[SERVER] WELCOME TO SERVER + " + new Date());
+//		dout.flush();
+//		
+//		server.close();
+//		System.out.println("[INFO] SERVER EXIT");
 		
-		Socket client = server.accept();	// 1회
-		OutputStream out = client.getOutputStream();
-		DataOutputStream dout = new DataOutputStream(out);
-		System.out.println("접속자 IP : " + client.getInetAddress());
-		dout.writeUTF("[SERVER] WELCOME TO SERVER + " + new Date());
-		dout.flush();
-		
-		server.close();
-		System.out.println("[INFO] SERVER EXIT");
+		//루프 접속
+		while(true)
+		{
+			Socket client = server.accept();
+			OutputStream out = client.getOutputStream();
+			DataOutputStream dout = new DataOutputStream(out);
+			System.out.println("접속자 IP : " + client.getInetAddress());
+			dout.writeUTF("[SERVER] WELCOM TO SERVER + " + new Date());
+			dout.flush();
+			dout.close();
+			out.close();
+			client.close();
+		}
 
 	}
 
