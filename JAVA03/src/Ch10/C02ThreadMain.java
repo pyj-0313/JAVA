2e765f470f66248324c061561c79fd4056e89f0b
pyj-0313 <pyjun0313@gmail.com>
2026-05-12 12:23:46 +0900

    16 DAY - JAVA03(C02ThreadMain)

---
+++ JAVA03/src/Ch10/C02ThreadMain.java
@@ -0,0 +1,52 @@
+package Ch10;
+
+public class C02ThreadMain {
+
+	public static void main(String[] args) {
+		
+		
+		// 1 Runnable Interface 를 이용한 스레드 분할
+		
+		// 1)
+		C02Worker1 w1 = new C02Worker1();
+		C02Worker2 w2 = new C02Worker2();
+		
+		// 2)
+		Thread th1 = new Thread(w1);
+		Thread th2 = new Thread(w2);
+		
+		// 3)
+		th1.start();
+		th2.start();
+		
+		
+		// 2 Thread Class 를 이용한 스레드 분열
+		new Thread() {
+
+			@Override
+			public void run() {
+				
+				for(int i=0;i<5;i++) {
+					System.out.println("TASK03...");
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			}
+			
+			
+		}.start();
+		
+		
+		
+		
+		
+		// 2 Thread Class 를 이용한 스레드 분할
+
+	}
+
+}
