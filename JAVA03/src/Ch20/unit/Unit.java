10aabaa6973c21512198a85d1762b00dcc44aaf7
pyj-0313 <pyjun0313@gmail.com>
2026-05-15 17:12:32 +0900

    19 DAY - JAVA03(unit/Unit)

---
+++ JAVA03/src/Ch20/unit/Unit.java
@@ -0,0 +1,38 @@
+package Ch20.unit;
+
+public abstract class Unit {
+	int hp;
+	int amor;
+	String type;
+	public boolean isDead;
+	
+	abstract public void move();
+	abstract public void underAttack(int damage);
+	//getter
+	public int getHp() {
+		return hp;
+	}
+	public int getAmor() {
+		return amor;
+	}
+	public String getType() {
+		return type;
+	}
+	public boolean isDead() {
+		return isDead;
+	}
+	public void setDead(boolean isDead) {
+		this.isDead = isDead;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	public void setAmor(int amor) {
+		this.amor = amor;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
