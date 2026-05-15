24c5c1b534792b08076194c94eb8dbb5fed7b329
pyj-0313 <pyjun0313@gmail.com>
2026-05-15 17:13:34 +0900

    19 DAY - JAVA03(weapon/Pistol)

---
+++ JAVA03/src/Ch20/weapon/Pistol.java
@@ -0,0 +1,38 @@
+package Ch20.weapon;
+
+import Ch20.unit.Unit;
+
+public class Pistol extends Gun {
+
+	
+	public Pistol() {
+		this.power = 10;
+		this.curBuillitCnt=0;
+		this.maxBuillitCnt=10;
+	}
+	@Override
+	public void fire(Unit unit) {
+		
+		if(curBuillitCnt==0) {
+			System.out.println("[경고] 재장전이 필요합니다");
+			return ;
+		}
+		
+		unit.underAttack(this.power);
+		curBuillitCnt--;
+		System.out.println("[정보] "+unit.getType() + " 에게 "+this.power+"만큼 피해를 입혔습니다. 현재HP : "+ unit.getHp() + "현재AMOR : " + unit.getAmor());	
+	}
+
+	@Override
+	public void reload(int bullit) {
+		
+		if(this.maxBuillitCnt>(bullit+this.curBuillitCnt))
+			this.curBuillitCnt+=maxBuillitCnt;
+		else
+			this.curBuillitCnt+=bullit;
+		
+		System.out.println("[정보] 재장전 완료");
+		
+	}
+
+}
