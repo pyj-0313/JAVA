2aaca8edc1bde676cdf51c73eeb3aa553ae3759f
pyj-0313 <pyjun0313@gmail.com>
2026-05-15 17:13:28 +0900

    19 DAY - JAVA03(weapon/Rifle)

---
+++ JAVA03/src/Ch20/weapon/Rifle.java
@@ -0,0 +1,36 @@
+package Ch20.weapon;
+
+import Ch20.unit.Unit;
+
+public class Rifle extends Gun {
+
+	public Rifle() {
+		this.power = 50;
+		this.curBuillitCnt=0;
+		this.maxBuillitCnt=100;
+	}
+	@Override
+	public void fire(Unit unit) {
+		
+		if(curBuillitCnt==0) {
+			System.out.println("[경고] 재장전이 필요합니다");
+			return ;
+		}
+		
+		unit.underAttack(this.power);
+		curBuillitCnt--;
+		System.out.println("[정보] "+unit.getType() + " 에게 "+this.power+"만큼 피해를 입혔습니다.");	
+	}
+
+	@Override
+	public void reload(int bullit) {
+		
+		if(this.maxBuillitCnt>(bullit+this.curBuillitCnt))
+			this.curBuillitCnt+=maxBuillitCnt;
+		else
+			this.curBuillitCnt+=bullit;
+		
+		System.out.println("[정보] 재장전 완료");
+		
+	}
+}
